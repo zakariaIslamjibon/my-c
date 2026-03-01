255a035d05dc1d8c348c0a859ad0335a70f74f1c
ZAKARIA ISLAM JIBON <101232383+zakariaIslamjibon@users.noreply.github.com>
2026-03-02 00:52:24 +0600

    Same Change

--- prtc.c
+++ prtc.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    char section;
-    int age;
-    double selary;
-    float bonus;
-    long double totle;
+  
+    int lapeYear;
 
-    
-    
+    printf("Enter Any Year: ");
+    scanf("%i", &lapeYear);
+
+    if ((lapeYear % 4 == 0 && lapeYear % 100 != 0) || (lapeYear % 400 == 0)){
+        printf("Its Leap Year\n");
+    }
+    else {
+        printf("Its Not Leap Year\n");
+    }
+
+    return 0;
 }

58ffc94f0b785065f90c1603cf2e85c8ec4350a2
ZAKARIA ISLAM JIBON <101232383+zakariaIslamjibon@users.noreply.github.com>
2026-03-02 12:54:12 +0600

    Same Change

--- loop.c
+++ loop.c
@@ -1,38 +1,41 @@
 #include <stdio.h>
-int main (){
-    int i;
-    for (i = 1 ; i <= 10; ++i){
-        printf("The Loop Number:%i\n", i);
 
+int main() {
+    int i, count, MyNum, sum;
+
+    // 1 for loop 1 থেকে 10 পর্যন্ত
+    for (i = 1; i <= 10; ++i){
+        printf("The Loop Number: %i\n", i);
     }
 
-    int MyNum , count , sum = 0;
-    printf("Enter Positive Number :") ;
-    scanf ("%i", &MyNum);
+    // 2️ for loop দিয়ে 1 থেকে MyNum পর্যন্ত যোগফল
+    sum = 0;
+    printf("Enter Positive Number: ");
+    scanf("%i", &MyNum);
 
-    for (count = 1 ; count <=MyNum ; ++count){
+    for (count = 1; count <= MyNum; ++count){
         sum += count;
     }
+    printf("The Sum using for loop: %i\n", sum);
 
-    printf(" The Number : %i", sum);
-
-     int i = 1 ;
+    // 3️ while loop 1 থেকে 10 পর্যন্ত
+    i = 1;
     while (i <= 10){
-        printf ("Loop:%i\n", i); 
+        printf("Loop using while: %i\n", i);
+        ++i;
     }
 
-    int count = 1 , MyNum, sum = 0 ;
-    printf ("Enter A Number:");
-    scanf ("%i", &MyNum);
+    // 4️ while loop দিয়ে 1 থেকে MyNum পর্যন্ত যোগফল
+    sum = 0;
+    count = 1;
+    printf("Enter A Number: ");
+    scanf("%i", &MyNum);
 
     while (count <= MyNum){
-         sum+=count;
-       
+        sum += count;
         ++count;
-       
-
     }
-     printf("The Number: %i\n", sum);
+    printf("The Sum using while loop: %i\n", sum);
 
     return 0;
 }

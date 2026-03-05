61f52e2852646ba3a4f5f0b01a390ba4218c08e4
ZAKARIA ISLAM JIBON <101232383+zakariaIslamjibon@users.noreply.github.com>
2026-03-05 19:48:33 +0600

    New change

--- psolve.c
+++ psolve.c
@@ -8,5 +8,47 @@ int main(){
         printf("%i", oddNum);
     }
 
+    //problem 2 input by user 1 to n , print the pluse value
+
+      int plusNum = 1, user, sum = 0 ;
+
+    printf ("Enter a Number :");
+    scanf ("%i", &user);
+
+    while (plusNum <=user){
+         sum+=plusNum;
+        ++plusNum;
+       
+
+    }
+    printf ("Pluse Number : %i", sum);
+
+    // problem 3 Factoril Number
+      
+    int fNum , input , sum = 1;
+
+    printf ("Enter A Number :");
+    scanf ("%i", &input);
+
+    for (fNum = 1 ; fNum <= input ; ++fNum){
+        sum*=fNum;
+    }
+    printf ("The Number : %i", sum);
+
+    // problem 4 
+    
+     int Num , input , sum = 1;
+
+     printf ("Enter A Number :");
+     scanf ("%i", &input);
+
+     for (Num = 1 <=10 ; Num<=input ; ++Num){
+        sum*=Num;
+     }
+     printf ("%i", sum);
+
+
+     
+
     return 0;
 }
